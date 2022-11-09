25418b3add4235572cee83c2fe7ef2dfeee78e47
Huỳnh Lê Đắc Tài <dactai2k2@gmail.com>
2022-11-09 07:46:31 +0700

    day-2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+import { Cart } from "./product/cart";
+import { ProductForm } from "./product/product-form";
+import { Products } from "./product/products";
+import { products } from "./products-data";
 
 function App() {
+  const [cart, setCart] = useState([]);
+  const onAddToCart = (product) => {
+    console.log("product info from App", product);
+
+    if (cart.find((p) => p.name === product.name)) {
+      setCart(
+        cart.map((p) => {
+          if (p.name === product.name) {
+            return {
+              ...p,
+              quantity: p.quantity + 1,
+            };
+          }
+          return p;
+        })
+      );
+    } else {
+      const cartProduct = {
+        ...product,
+        quantity: 1,
+      };
+
+      setCart([...cart, cartProduct]);
+    }
+  };
+
+  const onProductSubmit = (product) => {
+    console.log("created product", product);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductForm onSubmit={onProductSubmit} />
+      <Cart data={cart} />
+      <Products data={products} onAddToCart={onAddToCart} />
     </div>
   );
 }
---
+++ src/product/cart.js
@@ -0,0 +1,20 @@
+import { Product } from "./product";
+
+export const Cart = (props) => {
+  if (!props.data || props.data.length === 0) {
+    return (
+      <div>
+        <h2>Cart</h2>
+        Empty cart, please add some.
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h2>Cart</h2>
+      {props.data.map((product) => (
+        <Product key={product.name} {...product} />
+      ))}
+    </div>
+  );
+};
